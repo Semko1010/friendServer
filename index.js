722bf9b26b6d6d124e794ffce42c8370ad70764b
Semko1010 <semir01020@gmail.com>
2022-04-18 23:22:07 +0200

    v1.01

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 2020;
+const PORT = process.env.PORT || 2020;
 //imports
 const { userInfo } = require("./db_access/user_dao");
 
